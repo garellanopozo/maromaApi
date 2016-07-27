378be097662f34d785a5f041b7be2d325619aa24
Germán Arellano Pozo <garellano@comunidadandina.org>
2016-07-27 12:02:27 -0500

    changes.

--- maroma-domain/src/main/java/org/sertech/maroma/domain/CategoriaEntity.java
+++ maroma-domain/src/main/java/org/sertech/maroma/domain/CategoriaEntity.java
@@ -1,9 +1,6 @@
 package org.sertech.maroma.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import lombok.Data;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -15,6 +12,12 @@ import java.io.Serializable;
  */
 @Entity
 @Table(name = "categoria")
+@AttributeOverrides(value = {
+		@AttributeOverride(name = "createdBy", column = @Column(name = "usuario_creacion", nullable = false)),
+		@AttributeOverride(name = "createdDate", column = @Column(name = "fecha_creacion", nullable = false)),
+		@AttributeOverride(name = "lastModifiedDate", column = @Column(name = "ultima_fecha_modificacion", nullable = false)),
+		@AttributeOverride(name = "lastModifiedBy", column = @Column(name = "ultimo_usuario_modificacion", nullable = false)),
+		@AttributeOverride(name = "deleted", column = @Column(name = "borrado", nullable = false))})
 @Data
 @EntityListeners({ AuditingEntityListener.class })
 public class CategoriaEntity extends BaseAuditEntity implements Serializable {
--- maroma-domain/src/test/java/org/sertech/maroma/ProductoTest.java
+++ maroma-domain/src/test/java/org/sertech/maroma/ProductoTest.java
@@ -1,7 +1,39 @@
 package org.sertech.maroma;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.sertech.maroma.domain.ProductoEntity;
+import org.sertech.maroma.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
+
 /**
  * Created by garellano on 27/07/2016.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MaromaDomainApplication.class)
 public class ProductoTest {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @Test
+    public void selectitems(){
+        System.out.println("Ingreso de items");
+    }
+
+    @Test
+    public void addProducto() throws Exception {
+        ProductoEntity prod = new ProductoEntity();
+        prod.setProdCodigo("001");
+        prod.setProdDescripcion("jugo de Naranjas");
+        productoRepository.save(prod);
+        assertNotNull(prod.getId());
+        Thread.sleep(2000);
+        System.out.println(prod);
+    }
 }
+
