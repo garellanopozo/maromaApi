492ab0b3b85a2cb4def8f0da411c358871f9bbb6
Germán Arellano Pozo <garellano@comunidadandina.org>
2016-08-03 09:35:32 -0500

    changes

--- maroma-soa/src/main/java/org/sertech/maroma/rest/MaromaRestController.java
+++ maroma-soa/src/main/java/org/sertech/maroma/rest/MaromaRestController.java
@@ -5,12 +5,10 @@ import org.sertech.maroma.repository.ProductoRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
